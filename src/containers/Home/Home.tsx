6835b353be5e80a0e3c44006c763ddec39ac2351
Jennifer Goijman <jennifer.goijman@bourne-leisure.co.uk>
2020-10-21 09:23:46 +0200

    add some styles to Home

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,35 +1,41 @@
-import React from 'react';
-import { useHistory } from 'react-router-dom';
-import './Home.css';
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./Home.css";
 
-
-import useMouse from '@react-hook/mouse-position';
+import useMouse from "@react-hook/mouse-position";
 
 const Home = () => {
   const history = useHistory();
-  const verticalMode = window.innerWidth>1000?true:false;
+  const verticalMode = window.innerWidth > 1000 ? true : false;
   const ref = React.useRef(null);
   const mouse = useMouse(ref, {
     enterDelay: 100,
     leaveDelay: 100,
-  })
-  const mW: number = mouse.x?(mouse.x*100/window.innerWidth)-50:0;
+  });
+  const mW: number = mouse.x ? (mouse.x * 100) / window.innerWidth - 50 : 0;
 
   const moveMouse = {
-    transform: 'translateX(' + -mW/2 + '%)'
+    transform: "translateX(" + -mW / 2 + "%)",
+    transition: "transform 0.25s ease",
   };
 
   return (
     <div className="homeContainer">
-      <div className="links" ref={ref} style={verticalMode?moveMouse:{}} >
-        <div className="link" onClick={() => (history.push('/photos'))}>PHOTOS</div>
-        <div className="divider">{verticalMode?'-':''}</div>
-        <div className="link" onClick={() => (history.push('/work'))}>WORK</div>
-        <div className="divider">{verticalMode?'-':''}</div>
-        <div className="link" onClick={() => (history.push('/contact'))}>CONTACT</div>
+      <div className="links" ref={ref} style={verticalMode ? moveMouse : {}}>
+        <div className="link" onClick={() => history.push("/photos")}>
+          PHOTOS
+        </div>
+        <div className="divider">{verticalMode ? "-" : ""}</div>
+        <div className="link" onClick={() => history.push("/work")}>
+          WORK
+        </div>
+        <div className="divider">{verticalMode ? "-" : ""}</div>
+        <div className="link" onClick={() => history.push("/contact")}>
+          CONTACT
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
